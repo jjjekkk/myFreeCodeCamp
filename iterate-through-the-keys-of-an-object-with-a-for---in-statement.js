b9ecc571af68a69cca1fabff1f2f31884707b13a
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-04-07 00:11:00 +0500

    Add files via upload

---
+++ iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -0,0 +1,26 @@
+const users = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  // Only change code below this line
+  let usersOnline = 0;
+for (let user in usersObj) {
+  if (usersObj[user].online === true) {
+    usersOnline++
+  }
+}
+return usersOnline;
+
+  // Only change code above this line
+}
+
+console.log(countOnline(users));
