10519b1f53b1bace740fddb1299b5718f4663e6b
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-04-10 23:50:07 +0500

    Add files via upload

---
+++ find-the-longest-word-in-a-string.js
@@ -0,0 +1,14 @@
+function findLongestWordLength(str) {
+let strSplit = str.split(' ');
+let longestWord = 0;
+for (let i = 0; i<strSplit.length; i++)
+{if (strSplit[i].length > longestWord) {
+  longestWord = strSplit[i].length
+} 
+}
+
+
+  return longestWord;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
