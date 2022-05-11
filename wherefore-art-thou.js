f058066e9e3835b0bbb0dd1795daf192b63227f0
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-05-12 00:43:47 +0500

    Add files via upload

---
+++ wherefore-art-thou.js
@@ -0,0 +1,16 @@
+function whatIsInAName(collection, source) {
+  // Only change code below this line
+const sourceKeys = Object.keys(source);
+ return collection.filter(obj=> {
+   for (let i = 0; i<sourceKeys.length; i++) {
+     if (!obj.hasOwnProperty(sourceKeys[i]) ||
+     obj[sourceKeys[i]] !== source[sourceKeys[i]]){
+       return false
+     }
+   }
+     return true
+     });
+}
+  // Only change code above this line
+
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
