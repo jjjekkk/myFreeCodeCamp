e7c1d5795db4dfc0954e28f533022a6626159395
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-05-16 11:35:24 +0500

    Update telephone-number-validator.js

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -1,28 +1,20 @@
 function telephoneCheck(str) {
-  let firstTest = /[/(/)0-9]/g; 
- let firstSplit = str.split(' ');
- let arr = str.match(firstTest).join('');
+ const firstTest = /[/(/)0-9]/g; 
+ const firstSplit = str.split(' ');
+ const arr = str.match(firstTest).join('');
 
-let re1 = /^(1)\(\d{3}\)\d{7}$/; //1(555)
-let re2 = /^\(\d{3}\)\d{7}$/; //(555)
-let re3 = /^\d{10}$/; //555
-let re4 = /^(1)\d{10}$/; //1555
+const regExp =
+/^(1)\(\d{3}\)\d{7}$|^\(\d{3}\)\d{7}$|^\d{10}$|^(1)\d{10}$/;
 
 if(firstTest.test(str[0])===false ){ 
   //str[0] only number or '('
   return false
-  } else if(firstSplit[0].length===2) {
+} else if(firstSplit[0].length===2) {
     // test for country code
   return false
-} else if(re1.test(arr)===true) {
+} else if(regExp.test(arr)===true) {
   return true
-} else if(re2.test(arr)===true) {
-  return true
-}else if(re3.test(arr)===true) {
-  return true
-} else if(re4.test(arr)===true) {
-  return true
-} else  {
+} else {
   return false
   }
 }
