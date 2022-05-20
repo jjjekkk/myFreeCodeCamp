891ce2dfc2b1563abcddbe671d87d023636c8696
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-05-20 23:20:55 +0500

    Add files via upload

---
+++ sum-all-primes.js
@@ -0,0 +1,16 @@
+function sumPrimes(num) {
+  let isPrime = Array(num+1).fill(true);
+  isPrime[0]=false;
+  isPrime[1]=false;
+  for (let i = 2; i<Math.sqrt(num); i++) {
+    if (isPrime[i]) {
+      for (let j = i*i; j<=num; j += i)
+        isPrime[j] = false
+      }
+    }
+  
+  return isPrime.reduce(
+    (sum, prime, index)=> prime ? sum + index : sum, 0);
+}
+
+console.log(sumPrimes(10))
