30d4f7d1d03accccaacd5cf19a9ceeb7f9846afc
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-05-23 00:28:06 +0500

    Add files via upload

---
+++ binary-agents.js
@@ -0,0 +1,10 @@
+function binaryAgent(str) {
+let splitStr = str.split(' ');
+let newArr = [];
+for (let i = 0; i<splitStr.length; i++){
+newArr.push(String.fromCharCode(parseInt(splitStr[i], 2)))
+}
+return newArr.join('')
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
